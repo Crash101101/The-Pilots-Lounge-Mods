ffa0b6aae83301a017a7156bec37b3ad33f659dc
Crash101101 <53127867+Crash101101@users.noreply.github.com>
2019-07-21 18:16:03 -0400

    Skins for TPL
    Added Yak-52, and A4E skins

---
+++ TPL Modpack/liveries/A-4E-C/VMA-131 04 Crash/description.lua
@@ -0,0 +1,51 @@
+-- Livery Made by PorcoRosso86
+livery =
+{
+    {"Bottom", 0, "vma131_diamondbacks_bottom", false};
+    {"Top", 0, "vma131_diamondbacks_top", false};
+    {"Top", ROUGHNESS_METALLIC, "vma131_diamondbacks_RoughMet", false};
+   
+
+    -- flaps, used on most liveries
+    {"flaps_001", 0, "empty", true};
+    {"flaps_010", 0, "empty", true};
+    {"flaps_100", 0, "empty", true};
+
+    -- three digit, smaller nose numbers, used on most liveries
+    {"nose_001", 0, "a4e_bort_white", false};
+    {"nose_010", 0, "a4e_bort_white", false};
+    {"nose_100", 0, "empty", true};
+
+    -- two digit top-of-tail, used on most USMC liveries
+    {"tail_aggressor_001", 0, "empty", true};
+    {"tail_aggressor_010", 0, "empty", true};
+
+    -- three digit top-of-rudder, used on most USN liveries
+    {"rudder_001", 0, "empty", true};
+    {"rudder_010", 0, "empty", true};
+    {"rudder_100", 0, "empty", true};
+
+    -- wing bort used on most USN liveries
+    {"wing_001", 0, "empty", true};
+    {"wing_010", 0, "empty", true};
+    {"wing_100", 0, "empty", true};
+
+    -- 2-digit large nose numbers, used on USN/USMC aggressors
+    {"nose_aggressor_001", 0, "empty", true};
+    {"nose_aggressor_010", 0, "empty", true};
+
+    -- 3-digit large tail numbers, used on some USN/USMC aggressors
+    {"tail_001", 0, "empty", true};
+    {"tail_010", 0, "empty", true};
+    {"tail_100", 0, "empty", true};
+
+    {"fuel_150_a4e", 0, "uvMapsFuelTank_DFT150_DFT300_lightgrey", true};
+
+    {"fuel_300lr_a4e", 0, "uvMapsFuelTank_DFT150_DFT300_lightgrey", true};
+
+
+}
+
+name = "01 - VMA-131 04 Crash"
+
+countries = {"USA"}
---
+++ TPL Modpack/liveries/Yak-52/US Army Bird Dog Green Hi-Viz/description.lua
@@ -0,0 +1,73 @@
+livery = {
+
+	{"Yak52_Main", 0 ,"Yak52_Bird_Dog_Green_Hi_Viz",false};
+	{"Yak52_Main", ROUGHNESS_METALLIC ,"Yak52_Bird_Dog_Green_Hi_Viz_RoughMet",false};
+	{"Yak52_Main", 1 ,"Yak52_Main_noGofr_Normal",true};
+	
+	{"Yak52_Gear", 0 ,"Yak52_Gear_Bird_Dog_Green_Hi_Viz",false};
+	{"Yak52_Gear", ROUGHNESS_METALLIC ,"Yak52_Gear_RoughMet",true};
+	
+	
+	
+	
+	{"Yak52_Main_DECAL", 0 ,"Yak52_Bird_Dog_Green_Hi_Viz",false};
+	{"Yak52_Main_DECAL", ROUGHNESS_METALLIC ,"Yak52_Bird_Dog_Green_Hi_Viz_RoughMet",false};
+	{"Yak52_Main_DECAL", DECAL ,"empty",true};
+	{"Yak52_Main_DECAL", 1 ,"Yak52_Main_noGofr_Normal",true};
+	
+	
+	
+	
+	
+	{"Yak52_Main_BN_01", 0 ,"Yak52_Bird_Dog_Green_Hi_Viz",false};
+	{"Yak52_Main_BN_01", ROUGHNESS_METALLIC ,"Yak52_Bird_Dog_Green_Hi_Viz_RoughMet",false};
+	{"Yak52_Main_BN_01", DECAL ,"empty",true};
+	{"Yak52_Main_BN_01", 1 ,"Yak52_Main_noGofr_Normal",true};
+	
+	{"Yak52_Main_BN_10", 0 ,"Yak52_Bird_Dog_Green_Hi_Viz",false};
+	{"Yak52_Main_BN_10", ROUGHNESS_METALLIC ,"Yak52_Bird_Dog_Green_Hi_Viz_RoughMet",false};
+	{"Yak52_Main_BN_10", DECAL ,"empty",true};
+	{"Yak52_Main_BN_10", 1 ,"Yak52_Main_noGofr_Normal",true};
+	
+	{"Yak52_Main_BN_100", 0 ,"Yak52_Bird_Dog_Green_Hi_Viz",false};
+	{"Yak52_Main_BN_100", ROUGHNESS_METALLIC ,"Yak52_Bird_Dog_Green_Hi_Viz_RoughMet",false};
+	{"Yak52_Main_BN_100", DECAL ,"empty",true};
+	{"Yak52_Main_BN_100", 1 ,"Yak52_Main_noGofr_Normal",true};
+	
+	{"Yak52_Yakovlevs_BN_01", 0 ,"Yak52_Bird_Dog_Green_Hi_Viz",false};
+	{"Yak52_Yakovlevs_BN_01", ROUGHNESS_METALLIC ,"Yak52_Bird_Dog_Green_Hi_Viz_RoughMet",false};
+	{"Yak52_Yakovlevs_BN_01", DECAL ,"empty",true};
+	
+	{"Yak52_Yakovlevs_BN_10", 0 ,"Yak52_Bird_Dog_Green_Hi_Viz",false};
+	{"Yak52_Yakovlevs_BN_10", ROUGHNESS_METALLIC ,"Yak52_Bird_Dog_Green_Hi_Viz_RoughMet",false};
+	{"Yak52_Yakovlevs_BN_10", DECAL ,"empty",true};
+	
+	
+	
+	{"Yak52_PROP_0", 0 ,"Yak52_prop_r_00",true};	   
+	{"Yak52_PROP_1", 0 ,"Yak52_prop_r_01",true};	  
+	{"Yak52_PROP_2", 0 ,"Yak52_prop_r_02",true};	  
+	{"Yak52_PROP_3", 0 ,"Yak52_prop_r_03",true};	  
+	{"Yak52_PROP_4", 0 ,"Yak52_prop_r_04",true};	  
+	{"Yak52_PROP_5", 0 ,"Yak52_prop_r_05",true};
+	  	
+	{"Yak52_3BLADE_PROP_0", 0 ,"Yak52_prop3_00",true};	   
+	{"Yak52_3BLADE_PROP_1", 0 ,"Yak52_prop3_01",true};	   
+	{"Yak52_3BLADE_PROP_2", 0 ,"Yak52_prop3_02",true};	   
+	{"Yak52_3BLADE_PROP_3", 0 ,"Yak52_prop3_03",true};	   
+	{"Yak52_3BLADE_PROP_4", 0 ,"Yak52_prop3_04",true};	   
+	{"Yak52_3BLADE_PROP_5", 0 ,"Yak52_prop3_05",true};
+
+
+	
+
+}
+name = "US Army Bird Dog Green Hi-Viz"
+
+
+custom_args = 
+{
+ 
+ [70] = 0.0, -- Default Bort Number
+ 
+ }
